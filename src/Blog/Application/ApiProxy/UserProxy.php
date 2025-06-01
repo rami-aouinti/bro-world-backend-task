e36e422f8de5577327f13aedfb8f635697ebed12
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-06-01 23:20:20 +0200

    Add User Proxy

--- src/Blog/Application/ApiProxy/UserProxy.php
+++ src/Blog/Application/ApiProxy/UserProxy.php
@@ -37,7 +37,7 @@ readonly class UserProxy
     {
         $response = $this->httpClient->request('GET', "https://bro-world.org/api/v1/user", [
             'headers' => [
-                'Authorization' => 'ApiKey hunXGANRavVfBDaUpVvnSVS4X1proVA2FrhjAS1f',
+                'Authorization' => 'ApiKey eW1TKQcZCd8AWY6ZLJKURxYA6udwzJsZaW1sMWUF',
             ],
         ]);
 
