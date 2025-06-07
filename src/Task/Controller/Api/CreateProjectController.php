f8347ebb5566510abf64368e6efd247efa25c537
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-06-07 23:37:55 +0200

    Task Manager Api

---
+++ src/Task/Controller/Api/CreateProjectController.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Task\Controller\Api;
+
+use App\General\Infrastructure\ValueObject\SymfonyUser;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Projects\Application\Command\CreateProjectCommand;
+use App\Projects\Infrastructure\Service\DTO\ProjectInformationDTO;
+use App\Shared\Application\Bus\Command\CommandBusInterface;
+use App\Shared\Application\Service\UuidGeneratorInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\AuthenticatedVoter;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+/**
+ * Class ProjectController
+ *
+ * @package App\Task\Controller
+ * @author  Rami Aouinti <rami.aouinti@tkdeutschland.de>
+ */
+#[AsController]
+#[Route('/projects', name: 'platform_project.')]
+#[IsGranted(AuthenticatedVoter::IS_AUTHENTICATED_FULLY)]
+final readonly class CreateProjectController
+{
+    public function __construct(
+        private CommandBusInterface $commandBus,
+        private UuidGeneratorInterface $uuidGenerator
+    ) {
+    }
+
+    #[Route('/', name: 'create', methods: [Request::METHOD_POST])]
+    public function __invoke(SymfonyUser $user, ProjectInformationDTO $dto): JsonResponse
+    {
+        $command = new CreateProjectCommand(
+            $this->uuidGenerator->generate(),
+            $dto->name,
+            $dto->description,
+            $dto->finishDate
+        );
+
+        $this->commandBus->dispatch($command);
+
+        return new JsonResponse(['id' => $command->id], Response::HTTP_CREATED);
+    }
+}
---
+++ src/Task/Controller/Api/GetProjectsController.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Task\Controller\Api;
+
+use App\General\Infrastructure\ValueObject\SymfonyUser;
+use App\Projections\Application\Query\ProjectListQuery;
+use App\Projections\Infrastructure\DTO\ProjectListResponseDTO;
+use App\Shared\Application\Bus\Query\QueryBusInterface;
+use App\Shared\Application\Paginator\Pagination;
+use App\Shared\Infrastructure\Criteria\QueryCriteriaFromRequestConverterInterface;
+use App\Shared\Infrastructure\Criteria\RequestCriteriaDTO;
+use App\Shared\Infrastructure\Paginator\PaginationResponseDTO;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\Voter\AuthenticatedVoter;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+/**
+ * Class ProjectController
+ *
+ * @package App\Task\Controller
+ * @author  Rami Aouinti <rami.aouinti@tkdeutschland.de>
+ */
+#[AsController]
+#[Route('/projects', name: 'platform_project.')]
+#[IsGranted(AuthenticatedVoter::IS_AUTHENTICATED_FULLY)]
+final readonly class GetProjectsController
+{
+    public function __construct(
+        private QueryBusInterface $queryBus,
+        private QueryCriteriaFromRequestConverterInterface $converter
+    ) {
+    }
+
+    #[Route('/', name: 'getAll', methods: ['GET'])]
+    public function __invoke(RequestCriteriaDTO $criteria, SymfonyUser $user): JsonResponse
+    {
+        /** @var Pagination $pagination */
+        $pagination = $this->queryBus->dispatch(
+            new ProjectListQuery($this->converter->convert($criteria))
+        );
+
+        return new JsonResponse(PaginationResponseDTO::createFromPagination(
+            $pagination,
+            static fn (array $items) => ProjectListResponseDTO::createList($items)
+        ));
+    }
+}
